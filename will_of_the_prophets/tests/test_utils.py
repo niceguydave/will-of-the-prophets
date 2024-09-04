4033dea2e10bca494b3e77aebad2c371433391c6
pre-commit action <pre-commit-action@example.com>
2024-09-04 15:08:29 +0000

    pre-commit changes

--- will_of_the_prophets/tests/test_utils.py
+++ will_of_the_prophets/tests/test_utils.py
@@ -1,12 +1,13 @@
 import os
-import pytest
 from unittest.mock import patch
+
+import pytest
 
 from ..utils import get_boolean_env_variable
 
 
 @pytest.mark.parametrize(
-    "env_value, fallback_value, expected_result",
+    ("env_value", "fallback_value", "expected_result"),
     [
         ("1", False, True),
         ("0", True, False),
@@ -16,12 +17,12 @@ from ..utils import get_boolean_env_variable
         ("no", True, False),
     ],
 )
-def test_get_boolean_env_variable(env_value, fallback_value, expected_result):
+def test_get_boolean_env_variable(env_value, fallback_value, expected_result) -> None:
     with patch.dict(os.environ, {"TEST_VAR": env_value}):
         assert get_boolean_env_variable("TEST_VAR", fallback_value) == expected_result
 
 
-def test_get_boolean_env_variable_fallback():
+def test_get_boolean_env_variable_fallback() -> None:
     with patch.dict(os.environ, {}, clear=True):
         assert get_boolean_env_variable("TEST_VAR", True) == True
         assert get_boolean_env_variable("TEST_VAR", False) == False
